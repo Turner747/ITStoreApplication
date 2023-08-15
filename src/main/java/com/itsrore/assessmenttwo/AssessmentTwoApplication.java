f7ca23551eb12711223ab45a9367b4d24f0ef6cb
Joshua Turner <joshua.turner2021@gmail.com>
2023-08-15 23:30:12 +1000

    project created and database connect and user created

---
+++ src/main/java/com/itsrore/assessmenttwo/AssessmentTwoApplication.java
@@ -0,0 +1,13 @@
+package com.itsrore.assessmenttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsrore/assessmenttwo/AssessmentTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsrore.assessmenttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
