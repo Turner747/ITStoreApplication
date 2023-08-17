7178ddab226b17eb5ad424cc783ee2b7b29a6021
Joshua Turner <joshturner747@hotmail.com>
2023-08-17 21:04:11 +1000

    product controller complete

--- src/main/java/com/assessmenttwo/app/controller/ProductController.java
+++ src/main/java/com/assessmenttwo/app/controller/ProductController.java
@@ -1,7 +1,70 @@
 package com.assessmenttwo.app.controller;
 
+import com.assessmenttwo.app.model.Product;
+import com.assessmenttwo.app.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+
+    private ProductRepository repository;
+
+    public ProductController(ProductRepository repo){
+        repository = repo;
+    }
+
+    @GetMapping("/products")
+    public String productList(Model model){
+        List<Product> products = repository.findAll();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
+    @GetMapping("/products/add")
+    public String createProduct(Model model){
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "product-add";
+    }
+
+    @PostMapping("/products/add")
+    public String saveProduct(@ModelAttribute("product")Product product){
+        repository.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/{productId}")
+    public String viewProduct(@PathVariable("productId")Long productId, Model model){
+        Product product = repository.findById(productId).get();
+        model.addAttribute("product", product);
+        return "product-view";
+    }
+
+    @GetMapping("/products/{productId}/edit")
+    public String editProduct(@PathVariable("productId")Long productId, Model model){
+        Product product = repository.findById(productId).get();
+        model.addAttribute("product", product);
+        return "product-edit";
+    }
+
+    @PostMapping("/products/{productId}/edit")
+    public String updateProduct(@PathVariable("productId")Long productId, @ModelAttribute("product")Product product){
+        product.setId(productId);
+        repository.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/{productId}/delete")
+    public String deleteProduct(@PathVariable("productId")Long productId){
+        repository.deleteById(productId);
+        return "redirect:/products";
+    }
+    
 }
