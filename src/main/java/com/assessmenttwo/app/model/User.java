e6a8d58b8b0763d75c2fd73c607fdd1fda247edc
Joshua Turner <joshturner747@hotmail.com>
2023-08-22 18:29:28 +1000

    user authentication backend complete

--- src/main/java/com/assessmenttwo/app/model/User.java
+++ src/main/java/com/assessmenttwo/app/model/User.java
@@ -1,4 +1,23 @@
 package com.assessmenttwo.app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
 }
--- src/main/java/com/assessmenttwo/app/repository/UserRepository.java
+++ src/main/java/com/assessmenttwo/app/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.assessmenttwo.app.repository;
 import com.assessmenttwo.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository /*extends JpaRepository<User, Long> */{
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
